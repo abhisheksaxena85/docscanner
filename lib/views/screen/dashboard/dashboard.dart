8a7472c0885090e9618942fef189c7b53c0f9467
Abhishek Saxena <abhisheksaxena852828@gmail.com>
2025-04-25 21:42:34 +0530

    Project Architecture Setup

---
+++ lib/views/screen/dashboard/dashboard.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatefulWidget {
+  const Dashboard({super.key});
+
+  @override
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Dashboard'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Welcome to the Dashboard!',
+              style: TextStyle(fontSize: 24),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Add your button action here
+              },
+              child: const Text('Click'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
